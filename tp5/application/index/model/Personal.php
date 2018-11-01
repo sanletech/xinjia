80598dfcdc5dd74fb256d82263ae8cbb352dae35
shenadmin <445770173@qq.com>
2018-11-01 14:45:02 +0800

    11/1 14:44 港到港完成

--- tp5/application/index/model/Personal.php
+++ tp5/application/index/model/Personal.php
@@ -23,8 +23,14 @@ class Personal extends Model
                         ,$this->order_status['completion']) ;
 //        var_dump($order_status_on);exit;
         $order_status_on = implode(',', $order_status_on);
-        $statusSql ="(select a.* from (select * from hl_order_port_status where status in "
-                . "($order_status_on) order by mtime desc) a group by a.order_num)";
+        $where = "where status in ($order_status_on)";
+        $statusSql_1 ="(select b.* from  hl_order_port_status b right join"
+                . " (SELECT order_num , max(mtime) as mtime FROM `hl_order_port_status` ";
+        // 中间连接where 条件
+        $statusSql_2 = "  group by order_num) a"
+                . "  on a.mtime = b.mtime and a.order_num = b.order_num)";
+       $statusSql =$statusSql_1.$where.$statusSql_2;
+//        var_dump($statusSql);exit;
         $data =Db::name('order_port')->alias('OP')
                 ->join($statusSql.' OPS','OP.order_num = OPS.order_num','left')
                 ->join('hl_seaprice SP','SP.id=seaprice_id','left')
@@ -39,24 +45,45 @@ class Personal extends Model
                         . 'P1.port_code s_port_code, P1.port_name s_port,'
                         . 'P2.port_code e_port_code, P2.port_name e_port')
                 ->group('OP.order_num')->order('OP.ctime desc')->buildSql();
-        $lists =Db::table($data.' A')->where($map)->group('A.id')->limit($tol,$limit)->select();
+        $lists =Db::table($data.' A')->where($map)->group('A.id')->limit(1,100)->select();
 
         //展示扣柜驳回的理由
-        $container_buckle =Db::name('order_port_status')
-                ->where('status',$this->order_status['container_lock'])
-                ->order('mtime desc')->field('id,order_num,comment')->buildSql();
-        $container_buckle =Db::table($container_buckle.' A')->group('A.order_num');
-        foreach ($lists as $key =>$list){
+      
+       
+        $where = "where status =".$this->order_status['container_lock'];
+        $container_buckle = Db::table($statusSql =$statusSql_1.$where.$statusSql_2." A")->select();
+//        var_dump($container_buckle);exit;
+        foreach ($lists as $key =>$list){  
             foreach ($container_buckle as $k=>$v){
                 if($list['order_num']==$v['order_num']){
                     $lists[$key]['container_buckle_comment'] = $v['comment'];
                 }  
             }
+        }       
+        foreach ($lists as $key =>$list){  
             if(!array_key_exists('container_buckle_comment', $lists[$key])){
                 $lists[$key]['container_buckle_comment'] ='';
             }
+            switch ($list['status']){
+                case $this->order_status['stop']:
+                case $this->order_status['cancel']:
+                    $lists[$key]['status_title']='订单取消';
+                    break;
+                case $this->order_status['order_audit']:
+                case $this->order_status['booking_note']:
+                case $this->order_status['up_container_code']: 
+                case $this->order_status['sea_waybill']:
+                    $lists[$key]['status_title']='订单进行中';
+                    break;
+                case $this->order_status['completion']:
+                    $lists[$key]['status_title']='订单完成';
+                    break;
+                default :
+                    $lists[$key]['status_title']='订单未知错误';
+                    break;
+            }
         }       
-        $this->_p($lists);
+//        $this->_p($lists);exit;
         return  $lists;
         
     }
