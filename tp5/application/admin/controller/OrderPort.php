4bf3cd5bc4b854bc50a298f232b5664c5b2544d1
shenadmin <445770173@qq.com>
2018-11-17 18:29:21 +0800

    11/13 excel 时间处理bug

--- tp5/application/admin/controller/OrderPort.php
+++ tp5/application/admin/controller/OrderPort.php
@@ -165,8 +165,8 @@ class OrderPort extends Base
         // 在线付,月结,到港付 -
         // 上传订舱单,客户提交柜号,上传水运单
         //  -已经完成订单/未完成
-    //    $canshu = $this->request->param();
-    //    $this->_p($canshu);exit;
+        $canshu = $this->request->param();
+        $this->_p($canshu);exit;
         $map =[];
         //起运港口,目的港口
         $start_port = $this->request->param('stat_port');
