58c11661d6b22b6bd4957f343d836599dd857433
A-Teemo <649802052@qq.com>
2018-12-24 18:51:15 +0800

    账号分配权限

---
+++ tp5/application/admin/controller/Keeper2.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *  权限管理
+ */
+namespace app\admin\controller;
+use app\admin\common\Base;
+use think\Request;
+use think\Db;
+class Keeper extends Base
+{
+
+    //部门添加
+    public function addTeam(){
+        return $this->view->fetch(); 
+    }
+    public function addtoTeam(){
+    
+    }
+    
+     //部门修改
+    public function modifyTeam(){
+        
+        
+    }
+     //部门删除
+    public function deleteTeam(){
+        
+        
+    }
+    //部门查询
+    public function selectTeam(){
+        
+    }
+    
+    
+} 
