03cf46f81c847a95915b29bb90ed26341f3af22a
A-Teemo <649802052@qq.com>
2018-12-27 19:13:29 +0800

    分配权限

--- tp5/application/admin/controller/Jurisdiction.php
+++ tp5/application/admin/controller/Jurisdiction.php
@@ -13,7 +13,7 @@ class Jurisdiction extends Base
         return $this->view->fetch('Jurisdiction/control');
     }
     
-    
+    //信息列表
     public function userData($uid='') {
         //区域表
         $map =[];
@@ -30,7 +30,8 @@ class Jurisdiction extends Base
                         . "group_concat(distinct AG.id order by AG.id ) as power_id,"
                         . "group_concat(distinct P.port_name order by P.id) as port_name,"
                         . "group_concat(distinct P.port_code order by P.id) as port_code,"
-                        . "group_concat(distinct C.city order by C.id) as city_name ")
+                        . "group_concat(distinct C.city order by C.id) as city_name ,"
+                        . "group_concat(distinct C.city_id order by C.id) as city_code ")
                 ->group('U.id')->where($map)->select();
 
         foreach ($data as $key=> $value){
@@ -43,15 +44,18 @@ class Jurisdiction extends Base
                         ->where('id',$value['pid'])->find();
                 }
                 $data[$key]['department']=$parent_id['title'];
+                $data[$key]['department_id']=$parent_id['id'];
             }  else {
                 $data[$key]['department']= '';   
             }
- 
-            $data[$key]['power'] = explode(',', $value['power']);    
-            $data[$key]['power_id'] = explode(',', $value['power_id']);
-            $data[$key]['port_name'] = explode(',', $value['port_name']); 
-            $data[$key]['port_code'] = explode(',', $value['port_code']);    
-        }
+
+        $data[$key]['power'] = explode(',', $value['power']);    
+        $data[$key]['power_id'] = explode(',', $value['power_id']);
+        $data[$key]['port_name'] = explode(',', $value['port_name']); 
+        $data[$key]['port_code'] = explode(',', $value['port_code']); 
+        $data[$key]['city_name'] = explode(',', $value['city_name']); 
+        $data[$key]['city_code'] = explode(',', $value['city_code']); 
+    }
        
         if($uid){
             $count = 1;
@@ -64,10 +68,50 @@ class Jurisdiction extends Base
 
     }
     
+    //个人信息重新分配
+    public function userEdit() {
+        
+    }
+    
+    //部门列表
+    public function teamList() {
+        $data =Db::name('team')->select();
+        $arr = $this->generateTree($data);
+        return json($arr);
+        
+    }
+    //权限列表
+    public function authGroup() {
+        $data =Db::name('auth_group')->where('status',1)->field('id,title')->select();
+        return json($data);
+    }
     
     public function aaa() 
     {
         var_dump($this->request->param());exit;
     }
+    
+           //引用方式无限极
+    function generateTree($array){
+        //第一步 构造数据
+        $items = array();
+        $i=1;
+        foreach($array as $value){
+            $items[$i] = $value;
+            $i++;
+        }
+//        $this->_p($items);exit;
+        //第二部 遍历数据 生成树状结构
+        $tree = array();
+        foreach($items as $key => $item){
+            if(isset($items[$item['pid']])){
+                $items[$item['pid']]['childMenus'][] = &$items[$key];
+            }else{
+                $tree[] = &$items[$key];
+            }
+        }
+        return $tree;
+    }
+    
 
 } 
